13d3e5a2c91877dd6b14d84611da80431cac311f
Daniel Aboderin <aboderindaniel482@gmail.com>
2025-10-27 09:42:58 +0100

    fixed import errors

--- components/shared/products/productsGrid.jsx
+++ components/shared/products/productsGrid.jsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import ProductCard from "./ProductCard";
+import ProductCard from "./productCard";
 
 const container = {
   hidden: { opacity: 0 },
